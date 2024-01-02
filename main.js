9d2f39ffee0c692c5ca1f328d0ba3ddd170354b0
Harshil Garg <harshilgarg.mercury@gmail.com>
2024-01-02 21:02:45 +0530

    Added electron files

---
+++ main.js
@@ -0,0 +1,9 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadFile('index.html');
+});
