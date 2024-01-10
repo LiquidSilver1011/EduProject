c70b3a3c306a6c4d45d1f772feb577476eb8dd57
Loki <loku.cs.agrawal@gmail.com>
2024-01-10 17:05:10 +0530

    edited main.js to be more customizable with start page

--- main.js
+++ main.js
@@ -3,19 +3,21 @@ const { app, BrowserWindow } = require('electron');
 const url = require("url");
 const path = require("path");
 
-
-let mainWindow;
-
-app.on('ready', () => {;
-    mainWindow = new BrowserWindow({ 
-        width: 800, 
+function createMainWindeow(){ 
+    const mainWindow = new BrowserWindow({
+    width: 800, 
         height: 600,
         title: "test" 
     });
-    mainWindow.loadFile('home.html');
-});
 
-const starturl = url.format({
-    pathname:path.join(__dirname,"home.html"),
-    protocol:'file',
-})
+    const starturl = url.format({
+        pathname:path.join(__dirname,"home.html"),
+        protocol:'file',
+    })
+
+    mainWindow.loadURL(starturl);
+}
+
+app.whenReady().then(createMainWindeow);
+
+
