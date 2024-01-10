e84b2a3e09b51f97d6b876709a302320ad89b7be
Loki <loku.cs.agrawal@gmail.com>
2024-01-10 08:37:08 +0530

    package.json was referencing index.js instead of main.js added some code to main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // main.js
 const { app, BrowserWindow } = require('electron');
+const url = require("url");
+const path = require("path");
+
 
 let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow = new BrowserWindow({ 
+        width: 800, 
+        height: 600,
+        title: "test" 
+    });
     mainWindow.loadFile('home.html');
 });
 
+const starturl = url.format({
+    pathname:path.join(__dirname,"home.html"),
+    protocol:'file',
+})
