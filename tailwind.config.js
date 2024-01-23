8261e44f7a67ec807298cc64a943e17f2fbaef4b
Loki <loku.cs.agrawal@gmail.com>
2024-01-23 22:12:40 +0530

    asdf

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,20 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui:{
-    themes: [
-      {
-        mytheme: {
-        "primary": "#114084",
-        "secondary": "#3466AA",
-        "accent": "#a78bfa",
-        "neutral": "#82b7dc",
-        "base-100": "#F1f1f1",
-        "info": "#374151",
-        "success": "#2563eb",
-        "warning": "#fb923c",
-        "error": "#ef4444",
-        },
+    themes:[{
+    mytheme: {
+      "primary": "#114084",
+      "secondary": "#3466AA",
+      "accent": "#a78bfa",
+      "neutral": "#82b7dc",
+      "base-100": "#F1f1f1",
+      "info": "#374151",
+      "success": "#2563eb",
+      "warning": "#fb923c",
+      "error": "#ef4444",
       },
+    },
     ],
-
   },
 }
 
