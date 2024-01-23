c213d700ed124ace20a43687ac33818116ab08d5
Harshil Garg <harshilgarg.mercury@gmail.com>
2024-01-23 21:41:12 +0530

    Changed theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui:{
-    themes:["dark"]
+    themes: [
+      {
+        mytheme: {
+        "primary": "#114084",
+        "secondary": "#3466AA",
+        "accent": "#a78bfa",
+        "neutral": "#82b7dc",
+        "base-100": "#F1f1f1",
+        "info": "#374151",
+        "success": "#2563eb",
+        "warning": "#fb923c",
+        "error": "#ef4444",
+        },
+      },
+    ],
+
   },
 }
 
