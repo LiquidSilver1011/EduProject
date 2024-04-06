904dcf62861f831296515bafc56b2a2d19dca27d
Loki <loku.cs.agrawal@gmail.com>
2024-04-06 11:33:44 +0530

    deleting unecessary files

--- preload.js
+++
@@ -1,7 +0,0 @@
-const {contextBridge} = require("electron")
-const os = require("os");
-contextBridge.exposeInMainWorld("electron", {
-    homeDir: () => os.homedir(),
-    osVersion: () => os.version,
-    arch:()=>os.arch(),
-});
